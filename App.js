e724d7e1a71993d12be8f07e7f1f6646815f1313
Balintataw J <balintataw2@gmail.com>
2019-04-08 20:02:31 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
+import Slider from '@react-native-community/slider';
+
+import CircularProgressBar from './CircularProgressBar'
+
+type Props = {};
+export default class App extends Component<Props> {
+    state = {
+        percentage: 25
+    };
+    handleChangeEvent = (event) => {
+        this.setState({
+            percentage: event
+        });
+    }
+    render() {
+        return (
+        <View style={styles.container}>
+            <CircularProgressBar
+                strokeWidth="10"
+                sqSize="200"
+                percentage={this.state.percentage}/>
+                <Slider
+                    style={{width: 200, height: 60}}
+                    minimumValue={0}
+                    maximumValue={100}
+                    value={this.state.percentage}
+                    onValueChange={this.handleChangeEvent}
+                    minimumTrackTintColor="grey"
+                    maximumTrackTintColor="red"
+                />
+        </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
---
+++ CircularProgressBar.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Svg,{
+    Circle,
+    Ellipse,
+    G,
+    Text,
+    TSpan,
+    TextPath,
+    Path,
+    Polygon,
+    Polyline,
+    Line,
+    Rect,
+    Use,
+    Image,
+    Symbol,
+    Defs,
+    LinearGradient,
+    RadialGradient,
+    Stop,
+    ClipPath,
+    Pattern,
+    Mask,
+} from 'react-native-svg';
+
+
+class CircularProgressBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+  
+    render() {
+        // Size of the enclosing square
+        const sqSize = this.props.sqSize;
+        // SVG centers the stroke width on the radius, subtract out so circle fits in square
+        const radius = (this.props.sqSize - this.props.strokeWidth) / 2;
+        // Enclose cicle in a circumscribing square
+        const viewBox = `0 0 ${sqSize} ${sqSize}`;
+        // Arc length at 100% coverage is the circle circumference
+        const dashArray = radius * Math.PI * 2;
+        // Scale 100% coverage overlay with the actual percent
+        const dashOffset = dashArray - dashArray * this.props.percentage / 100;
+  
+        return (
+            <Svg
+                width={this.props.sqSize}
+                height={this.props.sqSize}
+                viewBox={viewBox}>
+                <Circle
+                    style={styles.circleBackground}
+                    fill='none'
+                    stroke='#ddd'
+                    cx={this.props.sqSize / 2}
+                    cy={this.props.sqSize / 2}
+                    r={radius}
+                    strokeWidth={`${this.props.strokeWidth}px`} />
+                <Circle
+                    style={styles.circleProgress}
+                    fill='none'
+                    stroke='red'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    cx={this.props.sqSize / 2}
+                    cy={this.props.sqSize / 2}
+                    r={radius}
+                    strokeWidth={`${this.props.strokeWidth}px`}
+                    // Start progress marker at 12 O'Clock
+                    transform={`rotate(-90 ${this.props.sqSize / 2} ${this.props.sqSize / 2})`}
+                    style={{
+                        strokeDasharray: dashArray,
+                        strokeDashoffset: dashOffset
+                    }} 
+                    />
+                <Text
+                    style={styles.circleText}
+                    fill="red"
+                    x="50%"
+                    y="50%"
+                    dy=".3em"
+                    textAnchor="middle">
+                    {`${this.props.percentage}%`}
+                </Text>
+            </Svg>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    circleText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    }
+});
+  
+CircularProgressBar.defaultProps = {
+    sqSize: 200,
+    percentage: 25,
+    strokeWidth: 10
+};
+  
+export default CircularProgressBar;
